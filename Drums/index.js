3f986be96a6fe587ef31e52ddf707630580d759a
Jakka Madhurima <jakka.madhurima2@mindtree.com>
2021-03-16 13:31:06 +0530

    Added Drums

---
+++ Drums/index.js
@@ -0,0 +1,12 @@
+
+function myFunction(e) {
+    const sound = document.querySelector(`audio[data-key="${e}"]`);
+    const key = document.querySelector(`.key[data-key="${e}"]`);  
+    if(!sound == null) return;
+    if(key == null) return;
+    
+    key.classList.add('playing');
+    sound.currentTime = 0;
+    sound.play();
+   
+}
